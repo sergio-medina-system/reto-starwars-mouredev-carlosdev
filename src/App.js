f81233a68eab219165780db0d892c71b248ffb96
Sergio Medina <sergiomsystem@gmail.com>
2023-02-05 14:23:06 -0500

    Add API request with its corresponding test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-import data from "./data.json";
+import { getPeople } from "./api/people";
 
 function App() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    getPeople().then((data) => setPeople(data.results));
+  }, []);
+
   return (
     <ul>
-      {data.results.map((character) => (
+      {people.map((character) => (
         <li key={character.name}>{character.name}</li>
       ))}
     </ul>
--- src/App.test.js
+++ src/App.test.js
@@ -3,15 +3,22 @@ import App from "./App";
 import data from "./data.json";
 
 describe("Star Wars APP", () => {
-  it("Should show a list of characters incluiding Luke Skywalker", () => {
-    render(<App />);
-    expect(screen.getByText("Luke Skywalker")).toBeInTheDocument();
+  beforeAll(() => {
+    jest.spyOn(window, "fetch");
   });
 
-  it("should show a list of characters from a JSON file", () => {
+  it("should show a list of characters from the API", async () => {
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => data,
+    });
+
     render(<App />);
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    expect(window.fetch).toHaveBeenCalledWith("https://swapi.dev/api/people/");
+
     for (let character of data.results) {
-      expect(screen.getByText(character.name)).toBeInTheDocument();
+      expect(await screen.findByText(character.name)).toBeInTheDocument();
     }
   });
 });
